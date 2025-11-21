800b75e7084e7529e68e26e8d06f8e2eb261eae0
saivivek <kalimerasaivivek33@gmail.com>
2025-11-21 18:03:04 +0530

    dev4change

--- strings.py
+++ strings.py
@@ -59,10 +59,113 @@ print(s[-1])"""
 """inp=input()
 ch=inp[-2]
 print(ch in 'aeiou')"""
-#slicing;
+#2.slicing;
 """obtaining a substracting from given string is called as slicing"""
-s='python'
+"""s='python'
 print(s[1:4:2])
+"""
+"""s='python'
+print(s[-5:-1])"""
+
+#write a program to check if given string is palindrome
+"""s=input("give a string ")
+r=s[ : :-1]
+print(s==r)"""
+#3.builtin functions
+"""
+1. len()-it is use to check no of element in any iterable
+s='saivivek'
+print(len(s))
+2. max() /min()
+they are used to return maximum and minimum element in given iterable
+s='saivivek'
+print(min(s))
+print(max(s))
+
+3.sorted()
+it gives a sorted list of all the elements in given iterable
+s='asdd'
+print(sorted(s))
+
+4. char()/ord()
+they are used for fetching characters and ascii values
+print(ord('a'))
+print(chr(65))"""
+
+#operation on string using function(methods)present in str class
+
+"""
+syntax:
+string_object.function_name()
+"""
+#case convertion methods
+"""s='python'
+s1=s.upper()
+print(s1)
+"""
+"""
+s='PyThoN'
+print(s.lower())
+print(s.swapcase())"""
+#validation methods
+"""s='adc'
+print(s.isalpha())
+print(s.isdigit())"""
+#searching methods
+"""s='abcdaa'
+print(s.index('a'))
+print(s.find('e'))
+print(s.rindex('a'))
+print(s.rfind('e'))"""
+#counting method
+"print(s.count('a'))"
+#splitting and joining methods
+"""s='1 2 3 4 5'
+print(s.split())
+s='1, 2, 3, 4, 5'
+print(s.split(','))
+1st=['this','is','python']
+print(" ".join(1st))"""
+#stripping methods
+"""s='   python   '
+print(s.strip(" "))"""
+#formatting methods
+"""s='my name is {}'
+inp=input()
+print(s.format('saivivek'))"""
+#write a program to check if given string can be a valid password
+#conditions:
+#password should have at least 4 characters and a max of 7 char
+#should always start with alphabet only
+#give o/p as true or false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
